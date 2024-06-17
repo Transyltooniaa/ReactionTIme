d7b07d548c27724a13241a8c614e5c8f7f80b177
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2024-06-18 01:43:14 +0530

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="container">
-    <h1>Ninja Reaction Timer</h1>
-    <button @click="start" :disabled="isPlaying">Start Game</button>
-    <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-    <Results v-if="showResults" :reactionTime="score"></Results>
-</div>
+      <h1>Ninja Reaction Timer</h1>
+      <button @click="start" :disabled="isPlaying">Start Game</button>
+      <p v-if="isPlaying">Wait for the green block to appear and click on it as fast as you can!</p>
+      <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+      <Results v-if="showResults" :reactionTime="score"></Results>
+    </div>
 </template>
 
 <script>
@@ -22,7 +23,8 @@ export default {
       isPlaying: false,
       delay : null,
       score : 0,
-      showResults : false
+      showResults : false,
+      preClick : false
     }
   },
   methods :{
@@ -50,5 +52,30 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+
+button{
+  background-color: #0faf87;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor:pointer;
+  border: none;
+  outline: none;
+}
+
+ /* push look of button down */
+button:active{
+  position:relative;
+  top:2px;
+}
+
+.container{
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+
 
 </style>
